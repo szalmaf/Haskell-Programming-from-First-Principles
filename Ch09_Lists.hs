43d19d3d0058b76511e013805a6ffd3dcf237d9f
szalmaf <szalmaf@gmail.com>
2016-11-07 15:53:29 -0500

    Add myLines and generalization, problem 2 an 3 solutions

---
+++ Ch09_Lists.hs
@@ -0,0 +1,33 @@
+Intermission: Exercises
+
+1)
+-- Break up String into a list os Strings (words) at the ' ' character
+
+factorial 0 = 1; factorial n = n * factorial (n-1)
+
+myWords "" = []; 
+myWords x = takeWhile (/= ' ') x : 
+       (myWords . 
+       dropWhile (== ' ') . 
+       dropWhile (/= ' ') 
+       $ x)
+
+2)
+-- Break up String at the '\n' character
+
+myLines "" = []; 
+myLines x = takeWhile (/='\n') x : 
+       (myLines . 
+       	dropWhile (=='\n') . 
+       	dropWhile (/='\n') 
+       	$ x)
+
+3)
+-- Parametrize on 
+
+myLines ch "" = []; 
+myLines ch x = takeWhile (/=ch) x : 
+               ((myLines ch) . 
+               	dropWhile (==ch) . 
+               	dropWhile (/=ch) 
+               	$ x)
