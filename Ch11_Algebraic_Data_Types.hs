9b1610ae5fcb68689ed2c00a9b78a0b0ee465dc1
Ferenc Szalma <szalmaf@gmail.com>
2016-11-19 21:12:05 -0500

    Add algebraic datatypes, typeconstructors, dataconstructors

--- Ch11_Algebraic_Data_Types.hs
+++ Ch11_Algebraic_Data_Types.hs
@@ -5,3 +5,114 @@
 --    4. type aliases
 --    5. "newtype"  
 
+
+-- 11.2 Data declarations
+
+-- Sum types, with more than one constructor
+data Bool = False | True -- Bool type
+data [] a = [] | a : [a] -- List type
+
+
+-- 11.3 Data and type constructor
+
+-- Similarly to how to check the *types* of functions w :t
+-- f function takes no argument
+let f = not True
+:t f
+f :: Bool
+
+-- f function w one argument
+let f x = x > 3
+:t f
+f :: (Ord a, Num a) => a -> Bool
+
+-- One can check *kinds* of types w :kinds
+:k Bool
+Bool :: *
+
+-- or
+:k [Int]
+[Int] :: *
+
+
+--  11.4 Data constructors and values
+
+data PugType = PugData -- const type & const value
+myPug = PugData :: PugType
+
+data HuskyType a = HuskyData -- a is *phantom* argument 
+myHusky :: HuskyType a
+myHusky = HuskyData
+
+data DogueDeBordeaux doge = DogueDeBordeaux doge
+myDoge :: DogueDeBordeaux Int
+myDoge = DogueDeBordeaux 10
+
+data Doggies a =
+      Husky a 
+    | Mastiff a
+    deriving (Eq, Show)
+:k Doggies -- kind of the type
+Doggies :: * -> * -- type needs to be applied to become concrete
+:t Husky -- type of Husky
+Husky :: a -> Doggies a
+
+-- Intermission exercises
+:k Doggies String
+Doggies String :: *
+
+:t Husky 10
+Husky 10 :: Num a => Doggies a
+
+Husky (10 :: Integer)
+Husky 10
+
+:t Husky (10 :: Integer)
+Husky (10 :: Integer) :: Doggies Integer
+
+:t Mastiff "Scooby Doo"
+Mastiff "Scooby Doo" :: Doggies [Char]
+
+:t DogueDeBordeaux
+DogueDeBordeaux :: doge -> DogueDeBordeaux doge
+
+:t DogueDeBordeaux  "doggie!"
+DogueDeBordeaux  "doggie!" :: DogueDeBordeaux [Char]
+
+:k DogueDeBordeaux
+DogueDeBordeaux :: * -> *
+
+
+-- 11.5 What's a type and what's data?
+
+data Price =
+    Price Integer deriving (Eq, Show)
+
+-- type w 3 possible values
+data Manufacturer =
+      Mini
+    | Mazda
+    | Tata
+      deriving (Eq, Show)
+
+-- type w 3 possible values
+data Airline =
+      PapuAir
+    | CatapultsR'Us
+    | TakeYourChancesUnited
+      deriving (Eq, Show)
+
+-- type w 2 possible values
+-- and dataconstructors taking arguments 
+data Vehicle = Car Manufacturer Price
+             | Plane Airline
+             deriving (Eq, Show)
+
+-- Intermission exercises
+
+myCar = Car Mini (Price 14000)
+myCar :: Vehicle
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False
