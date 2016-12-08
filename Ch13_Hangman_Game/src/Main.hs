79a19462e2eaf18ef3ffd21f472a808a8c8d6207
Ferenc Szalma <szalmaf@gmail.com>
2016-12-08 16:17:56 -0500

    Initial commit of Hangman Game

---
+++ Ch13_Hangman_Game/src/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+
+
+
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ Ch13_call-em-up/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Hello
+
+main =
+    sayHello ", Haskell!"
