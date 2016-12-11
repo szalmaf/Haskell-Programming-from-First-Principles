668aaf3a5709be16b7652003b5eb4c3ea765b5fd
Ferenc Szalma <szalmaf@gmail.com>
2016-12-11 17:41:34 -0500

    Add gameOver gameWin runGame and main fns

--- Ch13_Hangman_Game/src/Main.hs
+++ Ch13_Hangman_Game/src/Main.hs
@@ -85,3 +85,35 @@ handleGuess puzzle guess = do
          putStrLn "This character wasn't in the word, try again."
          return (fillInCharacter puzzle guess)
 
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+    if (length guessed) > 7 then
+        do putStrLn "You lose!"
+           putStrLn $ "The word was: " ++ wordToGuess
+           exitSuccess
+    else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+    if all isJust filledInSoFar then
+        do putStrLn "You win!"
+            exitSuccess
+        else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+    gameOver puzzle
+    gameWin puzzle
+    putStrLn $ "Current puzzle is: " ++ show puzzle
+    putStrLn "Guess a letter: "
+    guess <- getLine
+    case guess of
+        [c] -> handleGuess puzzle c >>= runGame
+        _   -> putStrLn "Your guess must be a single character"
+
+main :: IO ()
+main = do
+    word <- randomWord'
+    let puzzle = freshPuzzle (fmap toLower word)
+    runGame puzzle
+
