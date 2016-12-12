0bc15e9f5ec5674d2840dd5a34e927aa99e42f16
Ferenc Szalma <szalmaf@gmail.com>
2016-12-12 15:54:04 -0500

    Add finished palindrom code

--- Ch13_Palindrome.hs
+++ Ch13_Palindrome.hs
@@ -1,9 +1,11 @@
 import Control.Monad
+import System.Exit (exitSuccess)
 
 palindrome :: IO ()
 palindrome = forever $ do
     line1 <- getLine
     case (line1 == reverse line1) of
         True  -> putStrLn "It's a palindrome!"
-        False -> putStrLn "Nope!"
-        
+        False -> do
+            putStrLn "Nope!"
+            exitSuccess
