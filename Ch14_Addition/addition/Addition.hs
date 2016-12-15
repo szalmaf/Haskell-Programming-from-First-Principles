a62d612aa34425db048813dc26b7e0d5339a9b7b
Ferenc Szalma <szalmaf@gmail.com>
2016-12-14 23:07:51 -0500

    Add rnd Gen data type and QuickCheck test suite

--- Ch14_Addition/addition/Addition.hs
+++ Ch14_Addition/addition/Addition.hs
@@ -1,6 +1,63 @@
 module Addition where 
 
 import Test.Hspec
+import Test.QuickCheck
+
+oneThroughThree :: Gen Int  
+oneThroughThree = elements [1,2,3] -- choose from a list
+
+genBool :: Gen Bool
+genBool = choose (False, True) -- choose from a tuple
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a,b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a,b)
+-- sample (genTuple :: Gen ([()], Char))
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) =>
+                Gen (a,b,c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a,b,c) 
+   
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+    a <- arbitrary
+    elements [Nothing, Just a]
+-- sample' (genMaybe :: (Gen (Maybe Int)))
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+    a <- arbitrary
+    frequency [ (1, return Nothing)
+              , (3, return (Just a))]
+
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
+
 
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom = go num denom 0
