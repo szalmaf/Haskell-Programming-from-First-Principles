501167991eb85393ef22e232331f890dfdc2a151
Ferenc Szalma <szalmaf@gmail.com>
2016-12-18 15:56:32 -0500

    Add list monad examples

--- Ch18_Monad/Ch18_Monad.hs
+++ Ch18_Monad/Ch18_Monad.hs
@@ -52,3 +52,22 @@ twoBinds' =
               ++ name ++ " who is: "
               ++ age ++ " years.")
 
+twiceWhenEven :: [Integer] -> [Integer]
+twiceWhenEven xs = do
+    x <- xs
+    if even x
+        then [x*x, x*x]
+        else [x*x]
+
+twiceWhenEven' :: [Integer] -> [Integer]
+twiceWhenEven' xs = do
+    x <- xs
+    if even x
+        then [x*x, x*x]
+        else []
+
+twiceWhenEven'' :: [Integer] -> [Integer]
+twiceWhenEven'' xs = do
+    xs >>= \x -> if even x
+                    then [x*x, x*x]
+                    else []
