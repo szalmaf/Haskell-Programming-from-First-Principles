9937960b2759cc0aa9df29c6ce77eddc010b6eda
Ferenc Szalma <szalmaf@gmail.com>
2016-12-26 16:14:54 -0500

    Add prop and main to tests.hs

--- Ch14_Morse/morse/test/tests.hs
+++ Ch14_Morse/morse/test/tests.hs
@@ -5,7 +5,7 @@ import Morse
 import Test.QuickCheck
 
 allowedChars :: [Char]
-allowedChars = M.elems letterToMorse
+allowedChars = M.keys letterToMorse
 
 allowedMorse :: [Morse]
 allowedMorse = M.elems letterToMorse
@@ -14,5 +14,16 @@ charGen :: Gen Char
 charGen = elements allowedChars
 
 morseGen :: Gen Morse
-morse = elements allowedMorse
+morseGen = elements allowedMorse
+
+prop_thereAndBackAgain :: Property
+prop_thereAndBackAgain =
+    forAll charGen
+    (\c -> ((charToMorse c)
+        >>= morseToChar) == Just c)
+
+main :: IO ()
+main = quickCheck prop_thereAndBackAgain
+
+
 
