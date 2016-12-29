578ed23e748195816177bcc65e800859a5c3a9b0
Ferenc Szalma <szalmaf@gmail.com>
2016-12-29 12:23:32 -0500

    Add Semigroup and Arbitrary instance to Three and Four

--- Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
+++ Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
@@ -28,6 +28,27 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
   arbitrary = liftM2 Two arbitrary arbitrary
 type TwoAssoc a b = Two a b -> Two a b -> Two a b -> Bool
 
+data Three a b c = Three a b c deriving (Eq, Show)
+instance Semigroup (Three a b c) where
+  x <> y = x
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = liftM3 Three arbitrary arbitrary arbitrary
+type ThreeAssoc a b c = Three a b c -> Three a b c -> Three a b c -> Bool
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+instance Semigroup (Four a b c d) where
+  x <> y = x
+instance (Arbitrary a, 
+          Arbitrary b, 
+          Arbitrary c, 
+          Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = liftM4 Four arbitrary arbitrary arbitrary arbitrary
+type FourAssoc a b c d = 
+      Four a b c d
+  ->  Four a b c d 
+  ->  Four a b c d 
+  ->  Bool
+
 
 main :: IO ()
 main = do
@@ -35,3 +56,9 @@ main = do
   quickCheck (semigroupAssoc :: (IdentityAssoc Int))
   -- verboseCheck (semigroupAssoc :: (TwoAssoc Int String))
   quickCheck (semigroupAssoc :: (TwoAssoc Int String))
+  quickCheck (semigroupAssoc :: (ThreeAssoc Int Double String))
+  verboseCheck (semigroupAssoc :: (FourAssoc Double Int Int Int))
+
+
+
+
