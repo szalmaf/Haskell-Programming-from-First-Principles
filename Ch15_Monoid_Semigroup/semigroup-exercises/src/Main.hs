9cc67e8b0a6095cbc7809c185cbaff794b98a3f4
Ferenc Szalma <szalmaf@gmail.com>
2016-12-29 12:03:05 -0500

    Add Two a b semigroup instance

--- Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
+++ Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Semigroup
+import Control.Monad
 import Test.QuickCheck
 
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
@@ -20,8 +21,17 @@ instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = fmap Identity arbitrary
 type IdentityAssoc a = Identity a -> Identity a -> Identity a -> Bool
 
+data Two a b = Two a b deriving (Eq, Show)
+instance Semigroup (Two a b) where
+  x <> y = x
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = liftM2 Two arbitrary arbitrary
+type TwoAssoc a b = Two a b -> Two a b -> Two a b -> Bool
+
 
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivialAssoc)
   quickCheck (semigroupAssoc :: (IdentityAssoc Int))
+  -- verboseCheck (semigroupAssoc :: (TwoAssoc Int String))
+  quickCheck (semigroupAssoc :: (TwoAssoc Int String))
