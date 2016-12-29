59164a2188396e7d44fdc72fea4a78a4e044daaf
Ferenc Szalma <szalmaf@gmail.com>
2016-12-28 20:42:53 -0500

    Add Identity a semigroup instance with quickcheck

--- Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
+++ Ch15_Monoid_Semigroup/semigroup-exercises/src/Main.hs
@@ -3,20 +3,25 @@ module Main where
 import Data.Semigroup
 import Test.QuickCheck
 
-data Trivial = Trivial deriving (Eq, Show)
-
-instance Semigroup Trivial where
-  _ <> _ = Trivial
-
-instance Arbitrary Trivial where
-  arbitrary = return Trivial
-
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
+data Trivial = Trivial deriving (Eq, Show)
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
 type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+newtype Identity a = Identity a deriving (Eq, Show)
+instance Semigroup (Identity a) where
+  x <> y = x
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = fmap Identity arbitrary
+type IdentityAssoc a = Identity a -> Identity a -> Identity a -> Bool
 
 
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (semigroupAssoc :: (IdentityAssoc Int))
