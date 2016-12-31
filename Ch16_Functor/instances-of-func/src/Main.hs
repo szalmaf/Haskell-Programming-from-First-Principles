3fcf50e4e1c89c90fa6db12d6b42dbc8c1b236e1
Ferenc Szalma <szalmaf@gmail.com>
2016-12-31 12:47:40 -0500

    Add functorCompose' based on Fun; Add Identity a's Functor instance and quickcheck

--- Ch16_Functor/instances-of-func/src/Main.hs
+++ Ch16_Functor/instances-of-func/src/Main.hs
@@ -1,20 +1,48 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Main where
 
 import Test.QuickCheck
+import Test.QuickCheck.Function
+
 
 functorIdentity :: (Functor f, Eq (f a)) => 
   f a -> Bool
 functorIdentity f = 
   fmap id f == f
+
 functorCompose :: (Eq (f c), Functor f) =>
   (a -> b) -> (b -> c) -> f a -> Bool
 functorCompose f g x = 
   (fmap g (fmap f x)) == (fmap (g . f) x)
-li x = functorCompose (+1) (*2) (x :: [Int])
+li x = functorCompose (+1) (*2) (x :: [Int]) -- operates on concrete fns
+functorCompose' :: (Eq (f c), Functor f) =>
+       f a
+    -> Fun a b      -- arbitrarily generates f and g fns
+    -> Fun b c
+    -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) =
+  (fmap (g . f) x) == (fmap g . fmap f $ x)
+type IntToInt = Fun Int Int
+type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
+
+
+newtype Identity a = Identity a deriving (Eq, Show)
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = fmap Identity arbitrary
+type IdentityFC = Identity Int -> IntToInt -> IntToInt -> Bool
+
+
+
 
 main :: IO ()
 main = do
 
   quickCheck $ \x -> functorIdentity (x :: [Int])
   quickCheck li
-  
+  quickCheck (functorCompose' :: IntFC)
+
+  quickCheck $ \x -> functorIdentity (x :: (Identity Int))
+  quickCheck (functorCompose' :: IdentityFC)
