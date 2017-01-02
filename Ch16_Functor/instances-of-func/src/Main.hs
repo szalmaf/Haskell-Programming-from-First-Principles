1e5fe87515269e11a842e2cc715ebe13fa82baff
Ferenc Szalma <szalmaf@gmail.com>
2017-01-01 23:20:18 -0500

    Add Quant, K, Flip, List a, GoatLord a, TalkToMe a functors

--- Ch16_Functor/instances-of-func/src/Main.hs
+++ Ch16_Functor/instances-of-func/src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE Rank2Types #-} -- for Nat
+{-# LANGUAGE FlexibleInstances #-} -- for Flip
 
 module Main where
 
@@ -116,6 +117,70 @@ maybeToList :: Nat Maybe []
 maybeToList Nothing = []
 maybeToList (Just x) = [x]
 
+data Quant a b =
+    Finance
+  | Desk a
+  | Bloor b
+instance Functor (Quant a) where
+  fmap _ Finance   = Finance
+  fmap _ (Desk x)  = Desk x
+  fmap f (Bloor y) = Bloor (f y)
+
+data K a b =
+  K a
+instance Functor (K a) where
+  fmap _ (K x) = K x
+
+newtype Flip f a b =
+  Flip (f b a)
+  deriving (Eq, Show)
+instance Functor (Flip K a) where
+  fmap f (Flip (K x)) = Flip (K (f x))
+
+data EvilGoateeConst a b = -- What is/should be const here??
+  GoatyConst b
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst y) = GoatyConst (f y) 
+
+data LiftItOut f a =
+  LiftItOut (f a)
+instance Functor f => Functor (LiftItOut f) where
+  fmap fn (LiftItOut xs) = LiftItOut (fmap fn xs) -- Note the fmap on th right!!!
+
+data Parappa f g a =
+  DaWrappa (f a) (g a)
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap fn (DaWrappa xs ys) = DaWrappa (fmap fn xs) (fmap fn ys)
+
+data Notorious g o a t =
+  Notorious (g o) (g a) (g t)
+instance Functor g => Functor (Notorious g o a) where
+  fmap fn (Notorious xs ys zs) = Notorious xs ys (fmap fn zs)
+
+data List a =
+    Nil
+  | Cons  a (List a)
+instance Functor List where
+  fmap _ Nil         = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+data GoatLord a =
+    NoGoat
+  | OneGoat a 
+  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+instance Functor GoatLord where
+  fmap _ NoGoat               = NoGoat
+  fmap f (OneGoat x)          = OneGoat (f x)
+  fmap f (MoreGoats xs ys zs) = MoreGoats (fmap f xs) (fmap f ys) (fmap f zs)
+
+data TalkToMe a =
+    Halt
+  | Print String a 
+  | Read (String -> a)
+instance Functor TalkToMe where
+  fmap _ Halt         = Halt
+  fmap f (Print s x)  = Print s (f x)
+  fmap f (Read fn)    = Read (fmap f fn)
 
 
 main :: IO ()
