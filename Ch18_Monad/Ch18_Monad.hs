c257ca1f3a27aaa38385fb9fabfe3981bf737e83
Ferenc Szalma <szalmaf@gmail.com>
2017-01-09 11:20:31 -0500

    Add mkSphericalCow monad 3 different ways: case pattern matching, do monadic syntax, and >>= binding monadic syntax

--- Ch18_Monad/Ch18_Monad.hs
+++ Ch18_Monad/Ch18_Monad.hs
@@ -91,11 +91,64 @@ twiceWhenEven' xs = do
 
 twiceWhenEven'' :: [Integer] -> [Integer]
 twiceWhenEven'' xs = do
-    xs >>= \x -> if even x
+    xs  >>= \x -> (if even x
                     then [x*x, x*x]
-                    else []
+                    else [1]) 
+        >>= \x -> [3*x]
 
 -- Maybe monad examples
+
+data Cow = Cow  {
+                    name :: String
+                    , age :: Int
+                    , weight :: Int
+                } deriving (Eq, Show)
+
+noEmpty :: String -> Maybe String 
+noEmpty "" = Nothing
+noEmpty str = Just str
+
+noNegative :: Int -> Maybe Int 
+noNegative n    
+        | n >= 0 = Just n
+        | otherwise = Nothing
+
+-- if Cow's name is Bess, must be under 500
+weightCheck :: Cow -> Maybe Cow 
+weightCheck c =
+    let w = weight c 
+        n = name c
+    in if n == "Bess" && w > 499 
+        then Nothing
+        else Just c
+
+mkSphericalCow :: String -> Int -> Int -> Maybe Cow 
+mkSphericalCow name' age' weight' =
+    case noEmpty name' of 
+        Nothing -> Nothing 
+        Just nammy ->
+            case noNegative age' of 
+                Nothing -> Nothing 
+                Just agey ->
+                    case noNegative weight' of 
+                        Nothing -> Nothing
+                        Just weighty ->
+                            weightCheck (Cow nammy agey weighty)
+
+mkSphericalCow' :: String -> Int -> Int -> Maybe Cow 
+mkSphericalCow' name' age' weight' = do
+    nammy <- noEmpty name'
+    agey <- noNegative age'
+    weighty <- noNegative weight' 
+    weightCheck (Cow nammy agey weighty)
+
+mkSphericalCow'' :: String -> Int -> Int -> Maybe Cow 
+mkSphericalCow'' name' age' weight' =
+    noEmpty name' >>= \ nammy ->
+        noNegative age' >>= \ agey ->
+            noNegative weight' >>= \ weighty ->
+                weightCheck (Cow nammy agey weighty)
+
 f :: Integer -> Maybe Integer
 f 0 = Nothing
 f n = Just n
